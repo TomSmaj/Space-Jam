ba19d6c20196dd96e02c93d43acdf1ad2630e81d
TomSmaj <thomas.smajstrla@gmail.com>
2018-09-24 20:06:03 -0500

    created user/host login

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,8 @@ let testUser = {
 }
 */
 
+//varaibles from an object from firebase are temporairly transferred to this object
+//the getHtml function us then used to write the HTML containing all the information for a post to the document
 let postObject = {
     postId: 1000,
     host: 'testHost',
@@ -82,42 +84,35 @@ let postObject = {
     info: 'This is a test space. Please rent me.',
     availability: 'Every Saturday',
     status: false,
+    phone: "",
+    gKey: "AIzaSyAF8WmkI7S-sD3r40t29wi15vs4Czp60Go",
 
     getHtml: function () {
-        return `<div class="row">
-        <div class="col-sm-1"></div>
-
-        <div class="postingArea col-sm-10">
-            <h2 class="text-center"><p>${this.title}</p></h2>
-            <hr>
-            <div class="row">
-                <div class="landingPageGreeting">
-                    <h5 class="text-center"><p>Description: ${this.info}</p></h5>
-                </div>
+        return `<div class="row postArea">
+        <div class="row text-center">
+            <div class="jamSpace col-sm-6">
+                [space img]
             </div>
-
-
-            <div class="row text-center">
-                <div class="jamSpace col-sm-6">
-                    [space img]
-                </div>
-                <div class="bookingInfo col-sm-6">
-
-                    <p>Address: ${this.address}</p>
-                    <p>Phone: ${this.phone}</p>
-                    <p>Price: ${this.price}</p>
-                    <p>Size: ${this.size}</p>
-                </div>
+            <div class="bookingInfo col-sm-6">
+                <p>address: ${this.address} </p>
+                <p>phone number: ${this.phone} </p>
+                <p>price: ${this.price} </p>
             </div>
+        </div>
 
-            <div class="row text-center">
-                <div class="googleMaps col-sm-6">
-                    [google map api]
-                </div>
-                <div class="bookingConfirmation col-sm-6">
-                    <button type="button" class=" ${this.postId} btn btn-block">Book</button>
+        <div class="row text-center">
+            <div class="googleMaps col-sm-6">
+            <iframe
+                width="600"
+                height="450"
+                frameborder="0" style="border:0"
+                src="https://www.google.com/maps/embed/v1/place?key=${this.gKey}&q=${this.address}" allowfullscreen>
+            </iframe>
+          
+            </div>
+            <div class="bookingConfirmation col-sm-6">
+                <button type="button" class="${this.postId} btn btn-block">Book</button>
 
-                </div>
             </div>
         </div>
     </div>`
@@ -126,7 +121,6 @@ let postObject = {
 
 }
 
-let loggedUser = 'testUser';
 function updateContent() {
     //console.log('reached');
     postRef.once('value', function (snapshot) {
@@ -149,20 +143,25 @@ function updateContent() {
 }
 
 $(document).ready(function () {
-    loggedIn = true;
+    let loggedInObj;
+    loggedInObj = JSON.parse(localStorage.getItem("loggedInObj"));
+    console.log(loggedInObj);
 
-    //Waiting for user to click login    
-
-    //$(".loginBtn").on('click', function(){
-    //})
-
-
-    if (loggedIn) {
-        //updateNavBar();
-        //Display Username on Nav bar, change to log out button
-        updateContent();
-        //Display posts from FB (picture, google map, content, and book button)
+    if (loggedInObj[0]) {
+        if(loggedInObj[1] === "user"){
+            //updateNavBar();
+            //Display Username on Nav bar, change to log out button
+            updateContent();
+            //Display posts from FB (picture, google map, content, and book button)
+            console.log("user logged in");
+        }
+        else if(loggedInObj[1] === "host"){
+            console.log("host logged in");
+            //updateHostPosts();
+        }
+        else{console.log("not user or host");}
     }
+    else{console.log("not logged in");}
 
    
 
@@ -173,6 +172,8 @@ $(document).ready(function () {
         let postId = targ.attr('.postId');
         //Grab the content from Firebase - find which post has the ID, match with book Button, assign to renter
     })*/
+
+
 
 
 
